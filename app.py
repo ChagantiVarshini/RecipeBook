41626fff84570db78af8b5e7b0017b702661f460
ChagantiVarshini <varshinichaganti1115@gmail.com>
2025-08-27 20:36:37 +0530

    Updated features

--- app.py
+++ app.py
@@ -14,13 +14,16 @@ app.secret_key = "supersecretkey"
 
 load_dotenv()  # loads .env file
 
-mydb = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    port=os.getenv("DB_PORT")
-)
+try:
+    db = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT")
+    )
+except mysql.connector.Error as err:
+    print("Error: ", err)
 
 
 
